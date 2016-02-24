0df45a94877b130c8360d81b200fa0ed1f0a6e05
Joshua Nankin <josh@phaxio.com>
2016-02-23 20:24:29 -0600

    adding countdown clock

--- js/init68b3.js
+++ js/init68b3.js
@@ -1,38 +1,21 @@
 jQuery(document).ready(function () {
+    jQuery('.mobile-btn').click(function() {
+        jQuery('.mobile-nav').slideToggle();
 
-jQuery('.video-thumb').click(function() {
-	jQuery('.video-container').fadeIn();
-	jQuery('.video-container').append('<iframe width="1130" height="636" src="http://www.youtube.com/embed/S2EkDLqszHs?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-	jQuery('.video-thumb').hide();
-	jQuery(".video").fitVids();
-});
+    });
 
+    jQuery('.sticky-donate').click(function() {
+      jQuery('.modal').fadeToggle();
+    });
 
+    jQuery('.join-in-link').click(function() {
+      jQuery('.modal').fadeToggle();
+    });
 
-jQuery('.video-thumb').click(function() {
-	jQuery('.video-container-manifesto').fadeIn();
-	jQuery('.video-container-manifesto').append('<iframe width="1130" height="636" src="https://www.youtube.com/embed/I1gG2wrfqAM?autoplay=1" frameborder="0" allowfullscreen></iframe>');
-	jQuery('.video-thumb').hide();
-	jQuery(".video").fitVids();
-});
+    jQuery('.close').click(function() {
+      jQuery('.modal').fadeOut();
 
-jQuery('.mobile-btn').click(function() {
-	jQuery('.mobile-nav').slideToggle();
-
-});
-
-jQuery('.sticky-donate').click(function() {
-  jQuery('.modal').fadeToggle();
-});
-
-jQuery('.join-in-link').click(function() {
-  jQuery('.modal').fadeToggle();
-});
-
-jQuery('.close').click(function() {
-  jQuery('.modal').fadeOut();
-
-});
+    });
 
 });
 
